0ef6aa0f0607da43e61772cc4a5a5061f2b5da37
luismachado <luismachado@users.noreply.github.com>
2017-02-13 14:43:25 +0000

    PickerLabel extension
    Add fixed labels to your UIPickerViews

---
+++ PickerLabels.swift
@@ -0,0 +1,39 @@
+//
+//  PickerLabels.swift
+//  My Timer
+//
+//  Created by Luís Machado on 02/02/17.
+//  Copyright © 2017 LuisMachado. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIPickerView {
+   
+    func setPickerLabels(labels: [Int:UILabel], containedView: UIView) { // [component number:label]
+        
+        let fontSize:CGFloat = 20
+        let labelWidth:CGFloat = containedView.bounds.width / CGFloat(self.numberOfComponents)
+        let x:CGFloat = self.frame.origin.x
+        let y:CGFloat = (self.frame.size.height / 2) - (fontSize / 2)
+        
+        for i in 0...self.numberOfComponents {
+            
+            if let label = labels[i] {
+                
+                if self.subviews.contains(label) {
+                    label.removeFromSuperview()
+                }
+                
+                label.frame = CGRect(x: x + labelWidth * CGFloat(i), y: y, width: labelWidth, height: fontSize)
+                label.font = UIFont.boldSystemFont(ofSize: fontSize)
+                label.backgroundColor = .clear
+                label.textAlignment = NSTextAlignment.center
+                
+                self.addSubview(label)
+            }
+        }
+    }
+}
